e40f4674104bb8c245a6db056f509252c2226ce6
Alapan Das <25705483+alapanme@users.noreply.github.com>
2020-10-17 09:58:30 +0300

    Added test script for test retries

---
+++ cypress/integration/TC_15_numberOfRetries.spec.js
@@ -0,0 +1,10 @@
+describe('Test to demonstrate Test Retries in Cypress', () => {
+    before(() => {
+        cy.visit('https://wikipedia.org')
+    })
+
+    it('Validate Page Title', () => {
+        //Intentionally making it fail to check retries
+        cy.title().should('eq', 'Wikipedia1111')
+    })
+})
