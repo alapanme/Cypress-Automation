eb17795d627d131936798c8cb6945a6b43216493
Alapan Das <25705483+alapanme@users.noreply.github.com>
2021-02-14 22:44:29 +0200

    adding cypress studio

---
+++ cypress/integration/TC_18_cypressStudio.spec.js
@@ -0,0 +1,14 @@
+describe('Example to Demonstrate Cypress Studio', () => {
+
+    it('Extend Test via Cypress Studio', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/index.php/dashboard')
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('#divUsername > .form-hint').click();
+        cy.get('#txtUsername').type('Admin');
+        cy.get('#txtPassword').type('admin123');
+        cy.get('#btnLogin').click();
+        cy.get('#welcome').click();
+        cy.get('#welcome-menu > :nth-child(1) > :nth-child(2) > a').click();
+        /* ==== End Cypress Studio ==== */
+    })
+})
