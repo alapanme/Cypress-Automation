d288df6492bafe92a32a5c2d7d315f3fe0cf913d
Alapan Das <25705483+alapanme@users.noreply.github.com>
2021-02-17 08:52:58 +0200

    Added cypress intercept test for mocking api

---
+++ cypress/integration/TC_19_intercept.spec.js
@@ -0,0 +1,20 @@
+describe('Example to demonstrate API Mocking in Cypress using cy.intercept', () => {
+
+    beforeEach(() => {
+        cy.intercept('GET', '**/tags', { fixture: 'tags.json' })
+        cy.intercept('GET', '**/articles*', { fixture: 'articlefeed.json' })
+        cy.visit('https://angular.realworld.io/')
+    })
+
+    it('Mock the Tags from the API Response and then validate on UI', () => {
+        cy.get('.tag-list', { timeout: 10000 })
+            .should('contain', 'cypress')
+            .and('contain', 'selenium')
+    })
+
+    it('Mock the Article feed from the API Response and then validate on UI', () => {
+        cy.get('app-favorite-button.pull-xs-right').contains('10')
+        cy.get('.author').contains('testersdock')
+        cy.get('.preview-link > p').contains('This is a test description')
+    })
+})
