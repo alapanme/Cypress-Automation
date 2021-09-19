c796cd396d857959028fad4725e94c752fc08f3e
alapanme <25705483+alapanme@users.noreply.github.com>
2021-09-19 13:56:37 +0300

    common cypress jquery commands test and blog

---
+++ cypress/integration/TC_26_jqueryCommands.spec.js
@@ -0,0 +1,46 @@
+describe('Example to demonstrate commonly used JQuery commands in cypress', () => {
+
+    it('Check if a button is disabled or enabled', function () {
+        cy.visit('https://getbootstrap.com/docs/4.0/components/buttons/#disabled-state')
+        cy.get('button.btn.btn-lg.btn-primary').eq(2).then(($btn) => {
+            if ($btn.is(':disabled')) { cy.log('Button is disabled') }
+            else { cy.log('Button is enabled') }
+        })
+        cy.get('button.btn.btn-lg.btn-primary').eq(1).then(($btn) => {
+            if ($btn.is(':enabled')) { cy.log('Button is enabled') }
+            else { cy.log('Button is disabled') }
+        })
+    })
+
+    it('Remove the disabled attribute and validate that button is enabled now', function () {
+        cy.get('button.btn.btn-lg.btn-primary').eq(2).then(($btn) => {
+            cy.wrap($btn.removeAttr('disabled')).should('be.enabled')
+        })
+    })
+
+    it('Assert inner text', function () {
+        cy.visit('https://testautomationpractice.blogspot.com/')
+        cy.get('h2.title').eq(0).then(($ele) => {
+            expect($ele.text()).to.equal('New Windows')
+        })
+    })
+
+    it('Assert value', function () {
+        cy.get('input#field1').eq(0).then(($ele) => {
+            expect($ele.val()).to.equal('Hello World!')
+        })
+    })
+
+    it('Assert the value of an attribute', function () {
+        cy.get('#HTML7').then(($ele) => {
+            expect($ele.attr('data-version')).to.equal('1')
+        })
+    })
+
+    it('Assert CSS property', function () {
+        cy.visit('https://the-internet.herokuapp.com/tables')
+        cy.get('table#table1').then(($ele) => {
+            expect($ele.css('margin-bottom')).to.equal('20px')
+        })
+    })
+})
--- readme.md
+++ readme.md
@@ -41,6 +41,7 @@ Cypress is a next generation front end testing tool built for the modern web. Th
 - [How to perform Drag and Drop on HTML and Angular sites with Cypress](https://testersdock.com/cypress-drag-and-drop-html-angular/)
 - [How to handle new browser tab and window in Cypress](https://testersdock.com/cypress-new-window/)
 - [How to use filter(), find() and within() commands in Cypress](https://testersdock.com/cypress-filter-find-within/)
+- [Commonly used JQuery commands in Cypress](https://testersdock.com/cypress-jquery/)
 
 # Running All Specs
 - When you're running your tests from the Test Runner, make sure that none of the tests has `only`, as this basically tells cypress to run that particular test and skip all others.
