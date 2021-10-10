1f24a51e18a9c209f16774683bc5c5b66a9d96e2
alapanme <25705483+alapanme@users.noreply.github.com>
2021-10-10 21:34:44 +0300

    cypress recursion test and blog

---
+++ cypress/integration/TC_27_cypressRecursion.spec.js
@@ -0,0 +1,20 @@
+import { recurse } from 'cypress-recurse'
+
+describe('Example to demonstrate recursion in Cypress', () => {
+    it('Validate the number 3 using recursion', function () {
+        cy.visit('https://alapanme.github.io/random-number.html')
+        recurse(
+            () => {
+                cy.reload()
+                return cy.get('#myNumber').invoke('text').then(parseInt)
+            },
+            (x) => x === 3,
+            {
+                log: false,
+                timeout: 7000, // try up to 7 seconds
+                limit: 20, // try up to 20 times
+                delay: 50 // delay before next iteration, ms
+            }
+        ).should('equal', 3)
+    })
+})
