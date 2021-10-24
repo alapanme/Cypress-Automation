3beb13b1b0948bf09ddbf66fa2a42c0cefc3c795
alapanme <25705483+alapanme@users.noreply.github.com>
2021-10-24 12:22:42 +0300

    updated shadow dom test with includeShadowDom flag

--- cypress/integration/TC_14_shadowDOM.spec.js
+++ cypress/integration/TC_14_shadowDOM.spec.js
@@ -2,7 +2,7 @@ import TestFilters from '../support/filterTests.js'
 
 TestFilters([], () => {
     describe('Example to demonstrate the handling of Shadow Dom in Cypress', () => {
-        before(() => {
+        beforeEach(() => {
             cy.visit('https://books-pwakit.appspot.com/')
         })
 
@@ -16,6 +16,22 @@ TestFilters([], () => {
                 .type('Science')
                 .click()
                 .url('contains', 'explore?q=Science')
+        })
+
+        it('Input a text in the input box and after search validate the URL with includeShadowDom flag set to true', () => {
+            cy.get('book-app')
+                .find('#input')
+                .type('Science', { force: true })
+                .click()
+                .url('include', 'explore?q=Science')
+        })
+
+        it('Input a text in the input box and after search validate the URL with includeShadowDom option', () => {
+            cy.get('book-app')
+                .find('#input', { includeShadowDom: true })
+                .type('Science', { force: true })
+                .click()
+                .url('include', 'explore?q=Science')
         })
     })
 })
