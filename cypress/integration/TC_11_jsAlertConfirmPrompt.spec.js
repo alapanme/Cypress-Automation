edad1a03803b771802da4bcea667543ac4042ca5
alapanme <25705483+alapanme@users.noreply.github.com>
2022-03-07 20:44:47 +0200

    js alert test script and cypress version update

--- cypress/integration/TC_11_jsAlertConfirmPrompt.spec.js
+++ cypress/integration/TC_11_jsAlertConfirmPrompt.spec.js
@@ -1,43 +1,38 @@
-import TestFilters from '../support/filterTests.js'
+describe('Example to demonstrate handling of JavaScript Alerts, Confirm, Prompt in Cypress', () => {
 
-TestFilters([], () => {
-    describe('Example to demonstrate handling of JavaScript Alerts, Confirm, Prompt in Cypress', () => {
+    beforeEach(() => {
+        cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
+    })
 
-        beforeEach(() => {
-            cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
+    it('Handling JS Alert - Validate Alert Text and Click OK', () => {
+        cy.contains('Click for JS Alert').click()
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('I am a JS Alert')
         })
+        cy.on('window:confirm', () => true);
+        cy.get('#result').should('have.text', 'You successfully clicked an alert')
+    })
 
-        it('Handling JS Alert - Validate Alert Text and Click OK', () => {
-            cy.contains('Click for JS Alert').click()
-            cy.on('window:alert', (str) => {
-                expect(str).to.equal('I am a JS Alert')
-            })
-            cy.on('window:confirm', () => true);
-            cy.get('#result').contains('You successfuly clicked an alert')
+    it('Handling JS Confirm - Validate Confirm Text and Click OK', () => {
+        cy.contains('Click for JS Confirm').click()
+        cy.on('window:confirm', (str) => {
+            expect(str).to.equal(`I am a JS Confirm`)
         })
+        cy.on('window:confirm', () => true);
+        cy.get('#result').should('have.text', 'You clicked: Ok')
+    })
 
-        it('Handling JS Confirm - Validate Confirm Text and Click OK', () => {
-            cy.contains('Click for JS Confirm').click()
-            cy.on('window:confirm', (str) => {
-                expect(str).to.equal(`I am a JS Confirm`)
-            })
-            cy.on('window:confirm', () => true);
-            cy.get('#result').contains('You clicked: Ok')
+    it('Handling JS Confirm - Click Cancel', () => {
+        cy.contains('Click for JS Confirm').click()
+        cy.on('window:confirm', () => false);
+        cy.get('#result').should('have.text', 'You clicked: Cancel')
+    })
+
+    it('Handling JS Prompt - Input text in prompt, Click OK and Validate Input Text', () => {
+        cy.window().then(($win) => {
+            cy.stub($win, 'prompt').returns('This is a test text')
+            cy.contains('Click for JS Prompt').click()
         })
-
-        it('Handling JS Confirm - Click Cancel', () => {
-            cy.contains('Click for JS Confirm').click()
-            cy.on('window:confirm', () => false);
-            cy.get('#result').contains('You clicked: Cancel')
-        })
-
-        it('Handling JS Prompt - Input text in prompt, Click OK and Validate Input Text', () => {
-            cy.window().then(($win) => {
-                cy.stub($win, 'prompt').returns('This is a test text')
-                cy.contains('Click for JS Prompt').click()
-            })
-            cy.get('#result').contains('You entered: This is a test text')
-        })
-
+        cy.get('#result').should('have.text', 'You entered: This is a test text')
     })
 })
