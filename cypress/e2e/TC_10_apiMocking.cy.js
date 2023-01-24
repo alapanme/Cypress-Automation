82ba7de209e21c66c156e8be7147a4b4b2c0fc66
qadeval <devalforqa@gmail.com>
2023-01-24 23:21:04 +0530

    demo_test

--- cypress/e2e/TC_10_apiMocking.cy.js
+++ cypress/e2e/TC_10_apiMocking.cy.js
@@ -1,26 +1,24 @@
-import TestFilters from '../support/filterTests.js'
+import TestFilters from "../support/filterTests.js";
 
 TestFilters([], () => {
-    describe('Example to demonstrate API Mocking in Cypress', () => {
+  describe("Example to demonstrate API Mocking in Cypress", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route("GET", "**/tags", "fixture:tags.json");
+      cy.route("GET", "**/articles*", "fixture:articlefeed.json");
+      cy.visit("https://angular.realworld.io/");
+    });
 
-        beforeEach(() => {
-            cy.server()
-            cy.route('GET', '**/tags', 'fixture:tags.json')
-            cy.route('GET', '**/articles*', 'fixture:articlefeed.json')
-            cy.visit('https://angular.realworld.io/')
-        })
-
-        it('Mock the Tags from the API Response and then validate on UI', () => {
-            cy.get('.tag-list')
-                .should('contain', 'cypress')
-                .and('contain', 'selenium')
-
-        })
-
-        it('Mock the Article feed from the API Response and then validate on UI', () => {
-            cy.get('app-favorite-button.pull-xs-right').contains('10')
-            cy.get('.author').contains('testersdock')
-            cy.get('.preview-link > p').contains('This is a test description')
-        })
-    })
-})
+    it("Mock the Tags from the API Response and then validate on UI", () => {
+      cy.get(".tag-list")
+        .should("contain", "cypress")
+        .and("contain", "selenium");
+    });
+    /** it would be great if you implement page object model here  */
+    it("Mock the Article feed from the API Response and then validate on UI", () => {
+      cy.get("app-favorite-button.pull-xs-right").contains("10");
+      cy.get(".author").contains("testersdock");
+      cy.get(".preview-link > p").contains("This is a test description");
+    });
+  });
+});
